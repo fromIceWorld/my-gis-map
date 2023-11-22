8910016626a2b1ac26495d4b374dc440712a8b41
Cui Bingbing <752856108@qq.com>
2023-11-22 16:46:26 +0800

    feat:完善新的配置页

--- src/app/gis-config.js
+++ src/app/gis-config.js
@@ -1,7 +1,6 @@
 const GIS_CONFIG = {
-  className: 'GisMapComponent', // 暴露出的组件class名称【组件可以注册到window上，并把配置同时暴露】
-  html: {},
-  css: {},
+  className: "GisMapComponent", // 暴露出的组件class名称【组件可以注册到window上，并把配置同时暴露】
+  html: [],
   component: {
     event: [],
     methods: [],
